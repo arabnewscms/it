12f993cfd9d1420c022f98f435164e094a60a544
php anonymous <php.anonymous1@gmail.com>
2018-06-09 01:41:31 +0200

    Add files via upload

--- it/it_router.php
+++ it/it_router.php
@@ -4,12 +4,14 @@ view()->addLocation(__DIR__ .'/views');
 \Illuminate\Support\Facades\View::addNamespace('it', __DIR__ .'/views');
 
 Route::group(['namespace' => 'Phpanonymous\It\Controllers'],
+
 function () {
 
 		\Lang::addNamespace('it', __DIR__ .'/lang');
 
 		Route::get('/', function () {return it_views('welcome');});
 
+		Route::get('routelist', 'RouteList@RouteListData');
 		Route::get('workflow', 'WorkFlow@loading');
 		if (class_exists('Barryvdh\Elfinder\Session\LaravelSession')) {
 			Route::get('merge', 'Merge@merge');
@@ -21,12 +23,15 @@ function () {
 	});
 
 //////// Elfinder Configurations Routes ///////////////
-Route::group(['namespace'                   => 'Barryvdh\Elfinder'], function () {
-		Route::any('connector', ['as'             => 'elfinder.connector', 'uses'             => 'ElfinderController@showConnector']);
-		Route::get('popup/{input_id}', ['as'      => 'elfinder.popup', 'uses'      => 'ElfinderController@showPopup']);
-		Route::get('filepicker/{input_id}', ['as' => 'elfinder.filepicker', 'uses' => 'ElfinderController@showFilePicker']);
-		Route::get('tinymce', ['as'               => 'elfinder.tinymce', 'uses'               => 'ElfinderController@showTinyMCE']);
-		Route::get('tinymce4', ['as'              => 'elfinder.tinymce4', 'uses'              => 'ElfinderController@showTinyMCE4']);
-		Route::get('ckeditor', ['as'              => 'elfinder.ckeditor', 'uses'              => 'ElfinderController@showCKeditor4']);
+Route::group(['namespace' => 'Barryvdh\Elfinder'], function () {
+		if (class_exists('Barryvdh\Elfinder\ElfinderController')) {
+
+			Route::any('connector', ['as'             => 'elfinder.connector', 'uses'             => 'ElfinderController@showConnector']);
+			Route::get('popup/{input_id}', ['as'      => 'elfinder.popup', 'uses'      => 'ElfinderController@showPopup']);
+			Route::get('filepicker/{input_id}', ['as' => 'elfinder.filepicker', 'uses' => 'ElfinderController@showFilePicker']);
+			Route::get('tinymce', ['as'               => 'elfinder.tinymce', 'uses'               => 'ElfinderController@showTinyMCE']);
+			Route::get('tinymce4', ['as'              => 'elfinder.tinymce4', 'uses'              => 'ElfinderController@showTinyMCE4']);
+			Route::get('ckeditor', ['as'              => 'elfinder.ckeditor', 'uses'              => 'ElfinderController@showCKeditor4']);
+		}
 	});
 //////// Elfinder Configurations Routes ///////////////
