80e8e7808115e1e74a7531a77e04b2ba17a640a5
php anonymous <php.anonymous1@gmail.com>
2018-12-27 19:13:56 +0200

    Update navbar.blade.php

--- it/environment/resources/views/admin/layouts/navbar.blade.php
+++ it/environment/resources/views/admin/layouts/navbar.blade.php
@@ -4,7 +4,7 @@
             <div class="page-header-inner ">
                 <!-- BEGIN LOGO -->
                 <div class="page-logo">
-                    <a href="index.html">
+                    <a href="{{ aurl('') }}">
                         <img src="{{url('design/admin_panel')}}/assets/layouts/layout4/img/logo-light.png" alt="logo" class="logo-default" /> </a>
                     <div class="menu-toggler sidebar-toggler">
                         <!-- DOC: Remove the above "hide" to enable the sidebar toggler button on header -->
