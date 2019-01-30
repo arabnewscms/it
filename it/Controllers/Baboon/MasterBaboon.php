e23ebc753b5ec2fb0c2004dc073b93bf207e31be
php anonymous <php.anonymous1@gmail.com>
2019-01-30 11:55:30 +0200

    Update MasterBaboon.php

--- it/Controllers/Baboon/MasterBaboon.php
+++ it/Controllers/Baboon/MasterBaboon.php
@@ -161,7 +161,13 @@ protected $fillable = ['."\n";
 	}
 
 	public static function write($data, $filename, $namespace) {
-		$file = fopen(base_path(static ::$full_path.'/'.str_replace('\\', '/', str_replace('App','app',$namespace)).'/'.$filename.'.php', "w");
+		$file =
+		fopen(base_path(
+
+				static ::$full_path.'/'.str_replace('\\', '/', strtolower($namespace)))
+
+			.'/'.$filename.'.php', "w"
+		);
 		fwrite($file, $data);
 		fclose($file);
 	}
