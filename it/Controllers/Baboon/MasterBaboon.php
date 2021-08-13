eb9cf1a10cf4934cd147b646f7d03197da028f91
Mahmoud Ibrahim <phpanonymous@Mahmouds-MacBook-Pro.local>
2021-08-13 07:28:03 +0200

    it

--- it/Controllers/Baboon/MasterBaboon.php
+++ it/Controllers/Baboon/MasterBaboon.php
@@ -820,7 +820,7 @@ return [' . "\n";
 		<h3 class="card-title">{{!empty($title)?$title:\'\'}}</h3>
 		<div class="card-tools">
 			<button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-minus"></i></button>
-			<button type="button" class="btn btn-tool" data-card-widget="remove"><i class="fas fa-remove"></i></button>
+			<button type="button" class="btn btn-tool" data-card-widget="maximize"><i class="fas fa-expand"></i></button>
 		</div>
 	</div>
 	<!-- /.card-header -->
