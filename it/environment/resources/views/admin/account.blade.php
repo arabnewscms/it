5e16e23aff344ca46d33ae835c769618e160fa50
Mahmoud Ibrahim <phpanonymous@Mahmouds-MacBook-Pro.local>
2021-08-13 08:18:56 +0200

    it

--- it/environment/resources/views/admin/account.blade.php
+++ it/environment/resources/views/admin/account.blade.php
@@ -1,11 +1,11 @@
 @extends('admin.index')
 @section('content')
-<div class="card card-default">
+<div class="card card-dark">
     <div class="card-header">
         <h3 class="card-title">{{!empty($title)?$title:''}}</h3>
         <div class="card-tools">
             <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-minus"></i></button>
-            <button type="button" class="btn btn-tool" data-card-widget="remove"><i class="fas fa-remove"></i></button>
+            <button type="button" class="btn btn-tool" data-card-widget="maximize"><i class="fas fa-expand"></i></button>
         </div>
     </div>
     <!-- /.card-header -->
