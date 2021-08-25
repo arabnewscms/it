5fd92f161b14bdfc156636ab3b6fd225fbd3a541
Mahmoud Ibrahim <phpanonymous@Mahmouds-MacBook-Pro.local>
2021-08-25 16:04:11 +0200

    it

--- README.md
+++ README.md
@@ -35,7 +35,7 @@
 
 `Package Name: IT`
 
-`Version: 1.6.20`
+`Version: 1.6.21`
 
 `Offline Docs: 1.1` - [Online Docs](https://it.phpanonymous.com/docs)
 
--- it/helper/it.php
+++ it/helper/it.php
@@ -72,7 +72,7 @@ if (!function_exists('it_version_message')) {
 
 if (!function_exists('it_version')) {
 	function it_version() {
-		$version = '1.6.20';
+		$version = '1.6.21';
 		app()->singleton('it_version', function () use ($version) {
 			return $version;
 		});
