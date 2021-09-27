741623833c0ea229d0c0a6c9de58cb5340a2b9a3
Mahmoud Ibrahim <phpanonymous@Mahmouds-MacBook-Pro.local>
2021-09-27 05:21:28 +0200

    update

--- it/Controllers/Baboon/BaboonCreate.php
+++ it/Controllers/Baboon/BaboonCreate.php
@@ -124,7 +124,7 @@ class BaboonCreate extends Controller {
 			if ($r->input('col_type')[$i] == 'file') {
 				$store .= '$data[\'' . $conv . '\'] = "";' . "\n";
 			} elseif ($r->input('col_type')[$i] == 'time') {
-				$store .= '$data[\'' . $conv . '\'] = date(\'H:i\', strtotime(request(\'' . $conv . '\')));' . "\n";
+				$store .= '              $data[\'' . $conv . '\'] = date(\'H:i\', strtotime(request(\'' . $conv . '\')));' . "\n";
 			} elseif ($r->input('col_type')[$i] == 'date_time') {
 				$store .= '              $data[\'' . $conv . '\'] = date(\'Y-m-d H:i\', strtotime(request(\'' . $conv . '\')));' . "\n";
 			}
